1d0e89bf9cb46751a81f86656c7bc90b02f5d66d
Jennifer Gomez <jennifergoag@gmail.com>
2021-08-11 03:06:23 +0200

    first graphic in streamlit

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,29 +1,33 @@
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
 import numpy as np
 import pandas as pd
 from connection_sql import db
+import requests
 
-st.title('ANIMALS')
-query = f"""
-    SELECT * FROM animals;
-    """
+st.title('Hello, Bcn!')
 
-result = db.execute(query).fetchall()
+###################
+result = requests.get("http://localhost:5000/population_by_age").json()
 
 df = pd.DataFrame(
      result,
-     columns=['code_district', 'district', 'total'])
+     columns=['age', 'total'])
 
-st.vega_lite_chart(df, {
-    "height": {"step": 17},
-     'mark': {'type': 'bar' },
-     'encoding': {
-         'x': {"aggregate": "sum", 'field': 'total' },
-         'y': {'field': 'district'},
+st.vega_lite_chart(df, 
+{    
+    "title": {"text": "Population by Age", "anchor": "middle", "fontSize": 30, "color": ["#6C04A4"]},
+    "width": 800,
+    "height": 400,
+    "mark": {"type": "bar", "color": ["#6C04A4"]},
+    "encoding": {
+        'x': {"title": "Population", "aggregate": "sum", "scale": {"domain": [0,800000]}, 'field': 'total'},
+        'y': {"title": "Age", 'field': 'age'},
      },
      })
+####################
+
+
+
 
 st.title('POPULATION')
 query = f"""
